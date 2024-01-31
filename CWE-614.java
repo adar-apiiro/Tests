3ddcd91f4772a8d1044dd5a07a38b4a055f4d3b0
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-01-31 15:23:48 +0200

    Delete CWE-614.java

--- CWE-614.java
+++
@@ -1,7 +0,0 @@
-import javax.servlet.http.Cookie;
-
-String ACCOUNT_ID = "your_account_id";
-String acctID = "your_acct_id_value";
-
-Cookie c = new Cookie(ACCOUNT_ID, acctID);
-response.addCookie(c);
