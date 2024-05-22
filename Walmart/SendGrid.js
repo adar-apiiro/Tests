bb1992c13c81a15742c76c6f6e5ae0c6aa82b953
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-22 12:05:35 +0300

    Update SendGrid.js

--- Walmart/SendGrid.js
+++ Walmart/SendGrid.js
@@ -1,11 +1,7 @@
-// Require the SendGrid library
 const sgMail = require('@sendgrid/mail');
-const sendGridResponse = await sgMail.send(email);
 
-// Set your SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Create a message object containing the email details
 const msg = {
   to: 'test@example.com', // Recipient's email address
   from: 'you@yourdomain.com', // Your verified SendGrid email
@@ -14,7 +10,6 @@ const msg = {
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 
-// Send the email
 sgMail
   .send(msg)
   .then(() => {
